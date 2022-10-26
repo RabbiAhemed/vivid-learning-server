4b24f0c0c7de69d44ea816609e9c8290f2ef3726
Rabbi Ahmed Limon <rabbiahemed>
2022-10-26 15:22:57 +0600

    api for all courses

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const cors = require("cors");
 app.use(cors());
 const Port = process.env.Port || 5000;
 const courses = require("./Data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+// get all courses
+app.get("/allCourses", (req, res) => {
+  res.send(courses);
+});
